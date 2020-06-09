069b71bb7969000237fcabe96159c97bf48180f0
Patryk Krolikowski <krolikowskipatryk24@gmail.com>
2020-06-10 00:59:52 +0200

    postgres

--- core/settings.py
+++ core/settings.py
@@ -79,13 +79,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+if DEBUG == True:
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'Django',
+            'USER': 'postgres',
+            'PASSWORD': '!Ziomalos1992',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
